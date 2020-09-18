d1c765712f7edc905dffe622400bb0a7efd67cdc
wevlude <64749393+wevlude@users.noreply.github.com>
2020-09-18 15:27:03 +0300

    step_1
    step_1

---
+++ beginner_projects.py
@@ -0,0 +1,25 @@
+# Container
+PYHTON | BEGINNER PROJECTS
+
+PROJECT 1 : MAD LIB FOR YOU
+-------------------------
+
+print("MAD LIB GAME IS STARTING!")
+print("***************************")
+
+noun1=input("Enter a noun : ")
+noun2=input("Enter another noun : ")
+noun3=input("Enter another noun : ")
+noun4=input("Enter another noun : ")
+verb_ing=input("Enter a verb ending in ing : ")
+adj=input("Enter an adjective : ")
+fm=input("Enter a family member: ")
+verb=input("Enter a verb: ")
+number=input("Enter a number: ")
+ing=input("Enter an ingredient : ")
+
+print("\nEvery year we make " + noun1 +" at Chrismastime.\n" + verb_ing + " has been a tradition since I was a/an " +adj+" kid!")
+print( fm+" used to make most of the recipe back then but I would always help\n" + verb+" "+ing+ ".")
+print("Now that I am older,I make the entire batch of " + noun2 +" from scratch.\n All you have to do is mix " + noun3 + " and " +noun4+" in a bowl until fluffy and add "+noun1+".")
+print("Don't forget the " + noun3 +"!\n" + verb + " them on a " +noun2+" and bake them at "+number+" degrees.")
+print("After " + number +" minutes you will have a perfect " + noun4+"!")
